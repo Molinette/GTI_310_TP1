895ba2ff8e8d0c2bac8f17aa4fbbadb4ef4ed348
Benjamin Brulard <benjamin.brulard.1@ens.etsmtl.ca>
2018-01-29 15:38:13 -0500

    progression ImageBMP

--- model/ImageBMP.java
+++ model/ImageBMP.java
@@ -1,35 +1,85 @@
 package model;
 
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.nio.ByteBuffer;
 import javax.imageio.ImageIO;
 
 public class ImageBMP extends Image {
 
 	
-	BufferedImage BuffImg;
-	
-	public ImageBMP (File file) throws IOException{
+	FileInputStream input;
+	DataInputStream dataReader;
+		
+	public ImageBMP (File file) throws FileNotFoundException {
 		
 		super._file = file;	
-		BuffImg = ImageIO.read(file);
+		input = new FileInputStream(file);
+		
 		
 	}
 
 	public BufferedImage draw() {
-		return BuffImg;
+		byte[] dataT = new byte[4];
+		
+		
+		try {
+			input.read(dataT, 34, 4);
+			int tailleData = ByteBuffer.wrap(dataT).getInt();
+			byte[] data = new byte[tailleData];
+			
+			InputStream in = new ByteArrayInputStream(data);
+			BufferedImage bImageFromConvert = ImageIO.read(in);
+
+			ImageIO.write(bImageFromConvert, "jpg", new File(
+					"c:/new-darksouls.jpg"));
+			
+			
+			
+			for(int i = 0; i<tailleData; i++){
+				
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		//return buffImg;
 	}
 
 	public int getHeight() {
+		byte[] data = new byte[4];
 		
-		return BuffImg.getHeight();
+		try {
+			input.read(data, 22, 4);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return ByteBuffer.wrap(data).getInt();
 	}
 
 	public int getWidth() {
+		byte[] data = new byte[4];
 		
-	  return BuffImg.getWidth();
+		try {
+			input.read(data, 18, 4);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	  return ByteBuffer.wrap(data).getInt();
 	}
 }
 	
