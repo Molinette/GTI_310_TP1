57639f632a49ad0bc321cddf535d22c3b0f717fd
Monette-Molina <marc-alexandre.monette-molina.1@ens.etsmtl.ca>
2018-01-29 15:45:42 -0500

    ajout verification 24  bpp

--- controller/ImageFileFilter.java
+++ controller/ImageFileFilter.java
@@ -1,6 +1,10 @@
 package controller;
 
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import javax.swing.filechooser.FileFilter;
 
@@ -23,14 +27,39 @@ public class ImageFileFilter extends FileFilter {
 		String name = arg0.getName();
 		int pos = name.lastIndexOf(".") + 1;
 		
-		/* return TRUE if the file's extension matches the awaited extension */
-		return name.substring(pos).equals(EXTENSION);
+		if(name.substring(pos).equals(EXTENSION)){
+			try{
+				DataInputStream dataInputStream = new DataInputStream(new FileInputStream(arg0));
+				dataInputStream.skipBytes(28);
+				byte[] data = new byte[2];
+				dataInputStream.read(data);
+				dataInputStream.close();
+				
+				/* return TRUE if the file's extension matches the awaited extension and is 24 bpp */
+				if(ByteArrayToInt(data) == 24){
+					return true;
+				}
+			}
+			catch(IOException e){
+				
+			}
+		}
+		
+		return false;
 	}
 
 	
 	public String getDescription() {
 		/* This String will be displayed in the file type combobox */
-		return "BMP (*.bmp)";
+		return "24 BPP BMP (*.bmp)";
+	}
+	
+	public int ByteArrayToInt(byte[] byteArray){
+		int integer = 0;
+		for(int i = 0; i < byteArray.length-1 ; i++){
+			integer += (byteArray[i] & 0xff) << (8 * i); 
+		}
+		return integer;
 	}
 
 }
